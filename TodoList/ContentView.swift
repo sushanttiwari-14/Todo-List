83db9c01b67b05f590a04ab66eaf503094da8db0
Sushant Tiwari <sushanttiwari2005@gmail.com>
2026-01-02 18:39:14 +0530

    Initial Commit

---
+++ TodoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoList
+//
+//  Created by sushant tiwari on 02/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by sushant tiwari on 02/01/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
